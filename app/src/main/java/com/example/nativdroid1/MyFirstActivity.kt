2a62f664e42c2fd8dd36935e54c30ced8f4bf876
Juan Londoño <jelondono@sura.com.co>
2020-04-24 08:21:01 -0500

    Se limpia el codigo

--- app/src/main/java/com/example/nativdroid1/MyFirstActivity.kt
+++ app/src/main/java/com/example/nativdroid1/MyFirstActivity.kt
@@ -1,6 +1,5 @@
 package com.example.nativdroid1
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
--- app/src/main/java/com/example/nativdroid1/RelativeVsLinearActivity.kt
+++ app/src/main/java/com/example/nativdroid1/RelativeVsLinearActivity.kt
@@ -1,11 +1,9 @@
 package com.example.nativdroid1
 
-import android.content.DialogInterface.*
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_relative_vs_linear.*
-import kotlinx.android.synthetic.main.relative_layout_activity.*
 
 class RelativeVsLinearActivity : AppCompatActivity() {
 
@@ -13,25 +11,11 @@ class RelativeVsLinearActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relative_vs_linear)
 
-        //var user = getStringFromIntent("user")
-        //var pass = getStringFromIntent("pass")
         var user = intent.getStringExtra("user")
         var pass = intent.getStringExtra("pass")
 
         etName.setText(user)
         etPass.setText(pass)
-
-    }
-
-    private fun getStringFromIntent(intentKey: String): String {
-        if (intent.hasExtra(intentKey)) {
-            var extra: Bundle? = intent.extras
-            var text = extra?.get(intentKey)
-            if (text != null) {
-                return text.toString()
-            }
-        }
-        return ""
 
     }
 
